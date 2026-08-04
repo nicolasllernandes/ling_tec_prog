bfd8425547cc6debd5bccde0c4dec4c8a64af97e
Nícolas Leonardo <nicolasleonardo2008@gmail.com>
2026-08-04 09:25:55 -0300

    Add files via upload

---
+++ ex triangulo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	float base, altura, area;
+	
+	base = 10;
+	altura = 18;
+	area = base * altura / 2;
+	
+	printf("A area do triangulo de base %f e altura %f eh %f", base, altura, area);
+
+
+	return 0;
+}

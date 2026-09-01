1f0e47a360aa4e4d37bd1fdc0f7e1b67ded9cb56
Nícolas Leonardo <nicolasleonardo2008@gmail.com>
2026-09-01 09:32:53 -0300

    Add files via upload

---
+++ prática/resolvendo.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+	
+	int exercicio, A, B;
+	double notacao
+	printf("Lista de Exercícios: Selecione o exercício a ser executado (1-8)");
+	scanf("%d", &exercicio);
+	
+	switch (exercicio) {
+	
+		case 1:
+			printf("Insira um valor inteiro");
+			scanf("%d", &A);
+			
+			printf("Insira outro valor inteiro");
+			scanf("%d", &B);
+			
+			printf("%d %d", B, A);
+			break;
+		case 2:
+			printf("Insira um valor")
+			scanf("%lf", &notacao);
+			
+
+
+}		
+	return 0;
+}
